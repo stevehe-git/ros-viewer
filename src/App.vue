294ee1415e5d345e10e06cf341b39d2262709fe4
steve.hjx <steve.he@fjdynamics.com>
2025-12-29 18:37:20 +0800

    fix: 替换svg图标

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/robot.svg" class="logo" alt="robot logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/robot.svg" class="logo robot" alt="robot logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
